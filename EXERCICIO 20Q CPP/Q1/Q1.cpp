6e7b0965768d44ae7f19cda49de2cee88970e43c
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-05-18 16:21:15 -0300

    add exerc 20q

---
+++ EXERCICIO 20Q CPP/Q1/Q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int num;
+	
+	cout << "Digite um número!\n";
+	cin >> num;
+	
+	cout << "O antecessor desse numero é " << num - 1 << "\n" ;
+	
+	return 0;
+}
